7fc583d43969b41abe11bf3f535c7982ad4e8298
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2022-12-04 21:03:19 +0100

    Theme switch, no storing in storage

--- js/themeSwitch.js
+++ js/themeSwitch.js
@@ -1,7 +1,11 @@
 // import { themeSwitchElem } from "./main.js";
 
+// let currentTheme = 'dark';
+// sessionStorage.setItem = ('data-theme', currentTheme);
+let darkTheme = true;
+
 const lightThemeIcon = `<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#000"><path fill-rule="evenodd" d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" clip-rule="evenodd"></path></svg>`;
-const darkThemeIcon = `<svg width="18" height="18" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18" fill="currentColor" color="#eee"><path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path></svg>`;
+const darkThemeIcon = `<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" color="#000"><path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path></svg>`;
 
 
 const themeSwitchElem = document.querySelector('#switch');
@@ -9,7 +13,10 @@ const themeSwitchElem = document.querySelector('#switch');
 let themeSwitchIcon = themeSwitchElem.querySelector('span.icon');
 let themeSwitchText = themeSwitchElem.querySelector('span:last-child');
 
+
 themeSwitchElem.addEventListener('click', () => {
+    
+    console.log(darkTheme)
 
     if (themeSwitchText.textContent == 'Light Mode' ? 
         themeSwitchText.textContent = 'Dark Mode' : 
@@ -18,4 +25,9 @@ themeSwitchElem.addEventListener('click', () => {
     if (themeSwitchIcon.innerHTML == darkThemeIcon ? 
         themeSwitchIcon.innerHTML = lightThemeIcon : 
         themeSwitchIcon.innerHTML = darkThemeIcon);
+
+    if (darkTheme ? document.documentElement.setAttribute('data-theme', 'light') :
+                    document.documentElement.removeAttribute('data-theme', 'light'));
+
+    darkTheme = !darkTheme;
 });
